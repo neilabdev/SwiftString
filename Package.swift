f9c035d60b29283db81e184fe68f0e0f4fdbbef5
flangel <fll@scade.io>
2018-08-06 06:41:38 +0200

    Updated package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,29 @@
 // swift-tools-version:4.0
 
 import PackageDescription
+
 let package = Package(
-	name: "SwiftString",
-	dependencies: [
-	],
-	targets: [],
+    name: "SwiftString",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftString",
+            targets: ["SwiftString"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftString",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftStringTests",
+            dependencies: ["SwiftString"]),
+    ],
+
     swiftLanguageVersions: [4]
 )
